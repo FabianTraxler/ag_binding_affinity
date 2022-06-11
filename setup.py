c3556b4dd4edaf7ff0f500b5f8ee4976b12266d6
FabianTraxler <fabi.traxler@hotmail.com>
2022-06-11 14:38:13 +0200

    Add Package  Setup File

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,  find_packages
+
+setup(name='abag_affinity',
+      version='0.1',
+      description='Utilities and Model for Antibody-Antigen Affinity Prediction',
+      author='Fabian Traxler',
+      author_email='f.traxler11@gmail.com',
+      packages=find_packages())
