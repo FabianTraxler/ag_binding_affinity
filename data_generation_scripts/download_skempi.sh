f10cf3dd2b267ed4ce16f6117c58f0fe790972a2
FabianTraxler <fabi.traxler@hotmail.com>
2022-06-22 18:22:20 +0200

    Add SKEMPI-2 download script

---
+++ data_generation_scripts/download_skempi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ~/ag_binding_affinity
+
+mkdir ./data/SKEMPI_v2
+
+mkdir ./data/SKEMPI_v2/PDBs
+mkdir ./data/SKEMPI_v2/mutated_pdb
+
+cd ./data/SKEMPI_v2
+
+wget https://life.bsc.es/pid/skempi2/database/download/skempi_v2.csv
+
+wget https://life.bsc.es/pid/skempi2/database/download/SKEMPI2_PDBs.tgz
+
+tar -xvzf SKEMPI2_PDBs.tgz -C ./PDBs
