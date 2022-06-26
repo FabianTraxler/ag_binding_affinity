f72ff8dc91d360670d7b509dfaddae7c3aaa8cd3
Moritz Schaefer <mschaefer@d002.int.cemm.at>
2022-06-26 23:33:35 +0200

    running

---
+++ README.md
@@ -0,0 +1,2 @@
+# Installation
+Make sure to link the resources and results folders to the correct location (ln -s)
--- data_generation/download_and_transform_abdb.sh
+++ data_generation/download_and_transform_abdb.sh
@@ -2,12 +2,12 @@
 
 # We expect the repository to be in the home directory of the user
 # some configuration
-export ABAG_PATH=~/ag_binding_affinity
 
-cd ~/ag_binding_affinity/data_generation/AbDb
+# Using a proper snakemake-structure (e.g. one single main Snakefile in the project's root directory will avoid directory issues. Note you can also include additional snakemake rules using the include-directive in your Snakefile (use .smk as file extension. Syntax corresponds exactly to the main Snakefile))
+cd AbDb
 
 # download data
-scripts/download_abdb.sh -p ~/ag_binding_affinity/data/AbDb
+scripts/download_abdb.sh -p data/AbDb
 
 # transform the data
-snakemake --use-conda --cores 32
+snakemake --use-conda --cores 1
