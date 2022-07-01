b1177100e858657a0f1d423cc95a69c87c801eb0
FabianTraxler <fabi.traxler@hotmail.com>
2022-07-01 16:31:12 +0200

    Bug Fix in mutate and addition of summary df

--- data_generation/SKEMPI/scripts/relax.py
+++ data_generation/SKEMPI/scripts/relax.py
@@ -1,16 +1,10 @@
 # Inspiration from PyRosetta tutorial notebooks
 # https://nbviewer.org/github/RosettaCommons/PyRosetta.notebooks/blob/master/notebooks/06.08-Point-Mutation-Scan.ipynb
-import os
-import pandas as pd
-from typing import Dict, List
-from tqdm import tqdm
 from pathlib import Path
 import pyrosetta
-from pyrosetta.toolbox.mutants import mutate_residue
-from pyrosetta.rosetta.protocols.moves import Mover
 from pyrosetta.rosetta.core.pose import Pose, add_comment, dump_comment_pdb
 from pyrosetta.rosetta.protocols.relax import FastRelax
-
+import os
 
 pyrosetta.init(extra_options="-mute all")
 
@@ -22,8 +16,34 @@ relax.constrain_relax_to_start_coords(True)
 packer = pyrosetta.rosetta.protocols.minimization_packing.PackRotamersMover(scorefxn)
 
 
+def load_pose_without_header(pdb_path: str):
+    with open(pdb_path) as f:
+        lines = f.readlines()
+
+    i = 0
+    while True:
+        if lines[i][:4] != "ATOM":
+            i += 1
+        else:
+            break
+
+    new_path = pdb_path.split(".")[0] + "_tmp.pdb"
+
+    with open(new_path, "w") as f:
+        f.writelines(lines[i:])
+
+    pose = pyrosetta.pose_from_pdb(new_path)
+
+    os.remove(new_path)
+
+    return pose
+
+
 def load_pose(pdb_path: str) -> pyrosetta.Pose:
-    pose = pyrosetta.pose_from_pdb(pdb_path)
+    try:
+        pose = pyrosetta.pose_from_pdb(pdb_path)
+    except RuntimeError:
+        pose = load_pose_without_header(pdb_path)
     testPose = pyrosetta.Pose()
     testPose.assign(pose)
     return testPose
