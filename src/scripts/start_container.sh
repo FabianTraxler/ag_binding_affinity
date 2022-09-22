c82ac50ffae1246aaf73a89551344ea7d33ab7ad
Moritz Schaefer <mail@moritzs.de>
2022-09-22 17:22:27 +0200

    Make SLURM to use 100GB instead of 1TB

--- src/scripts/start_container.sh
+++ src/scripts/start_container.sh
@@ -1,1 +1,1 @@
-srun --container-image=nvcr.io#nvidia/pytorch:22.02-py3 -N1 -q a100 -c 100 --gres=gpu:a100:1 --mem=1TB --pty bash
+srun --container-image=nvcr.io#nvidia/pytorch:22.02-py3 -N1 -q a100 -c 100 --gres=gpu:a100:1 --mem=100GB --pty bash
