46033561610b0110c665f7eced4612b01915617a
FabianTraxler <fabi.traxler@hotmail.com>
2022-09-30 13:25:16 +0200

    Add Skempi validation set + new graph analyze tools

--- src/data_analysis/graph_analyze/graph_analyze.py
+++ src/data_analysis/graph_analyze/graph_analyze.py
@@ -1,17 +1,13 @@
 import os.path
+from typing import List, Dict
 import warnings
-from typing import Dict, List
-
 warnings.filterwarnings("ignore")
 import numpy as np
-from abag_affinity.dataset import BoundComplexGraphs
-from abag_affinity.utils.config import get_resources_paths, read_config
-from abag_affinity.utils.pdb_processing import (get_atom_edge_encodings,
-                                                get_atom_encodings,
-                                                get_distances,
-                                                get_residue_edge_encodings,
-                                                get_residue_encodings,
-                                                get_residue_infos)
+
+from abag_affinity.dataset import BoundComplexGraphs,DeepRefineBackboneInputs
+from abag_affinity.utils.config import read_config, get_resources_paths
+from abag_affinity.utils.pdb_processing import get_residue_infos, get_distances, get_residue_edge_encodings, \
+    get_atom_edge_encodings, get_atom_encodings, get_residue_encodings
 from abag_affinity.utils.pdb_reader import read_file
 
 
@@ -20,7 +16,7 @@ def write_selection_for_residue_interface_hull(pdb_id: str, interface_cutoff: in
     if use_dataloader:
         config = read_config("../../config.yaml")
         dataset = BoundComplexGraphs(config, "Dataset_v1", pdb_ids=[pdb_id],
-                                     interface_distance_cutoff=interface_cutoff, node_type="residue", load_from_disc=False,
+                                     interface_distance_cutoff=interface_cutoff, node_type="residue",
                                      save_graphs=False, force_recomputation=True)
 
         graph_dict = dataset.get_graph_dict(pdb_id)
@@ -37,8 +33,8 @@ def write_selection_for_residue_interface_hull(pdb_id: str, interface_cutoff: in
 
     elif pdb_path is not None and chain_id2protein is not None:
         structure, header = read_file(pdb_id, pdb_path)
-        structure_info, node_infos, residue_atom_coordinates = get_residue_infos(structure, header, chain_id2protein)
-        distances, closest_atoms = get_distances(node_infos, residue_distance=False, ca_distance=False)
+        structure_info, node_infos, residue_atom_coordinates = get_residue_infos(structure, chain_id2protein)
+        distances, closest_atoms = get_distances(node_infos, residue_distance=True, ca_distance=False)
 
 
         adjacency_matrix = get_residue_edge_encodings(distances, node_infos, chain_id2protein, interface_cutoff)
@@ -73,7 +69,7 @@ def write_selection_for_atom_interface_hull(pdb_id: str, interface_cutoff: int =
     if use_dataloader:
         config = read_config("../../config.yaml")
         dataset = BoundComplexGraphs(config, "Dataset_v1", pdb_ids=[pdb_id],
-                                     interface_distance_cutoff=interface_cutoff, node_type="atom", load_from_disc=False,
+                                     interface_distance_cutoff=interface_cutoff, node_type="atom",
                                      save_graphs=False, force_recomputation=True)
 
         graph_dict = dataset.get_graph_dict(pdb_id)
@@ -85,15 +81,13 @@ def write_selection_for_atom_interface_hull(pdb_id: str, interface_cutoff: int =
         interface_hull = np.where(adjacency_matrix[3, interface_nodes, :] < hull_size)
         interface_hull_nodes = np.unique(interface_hull[1])
 
-
-
         node_infos = graph_dict["residue_infos"]
         atom_encoding = graph_dict["node_features"]
         atom_names = graph_dict["atom_names"]
 
     elif pdb_path is not None and chain_id2protein is not None:
         structure, header = read_file(pdb_id, pdb_path)
-        structure_info, node_infos, residue_atom_coordinates = get_residue_infos(structure, header, chain_id2protein)
+        structure_info, node_infos, residue_atom_coordinates = get_residue_infos(structure, chain_id2protein)
         distances, closest_atoms = get_distances(node_infos, residue_distance=False, ca_distance=False)
         atom_encoding, atom_names = get_atom_encodings(node_infos, structure_info, chain_id2protein)
         adjacency_matrix = get_atom_edge_encodings(distances, atom_encoding, interface_cutoff)
@@ -109,8 +103,11 @@ def write_selection_for_atom_interface_hull(pdb_id: str, interface_cutoff: int =
         print("Please select 'use_dataloader' or provide path and info_dict")
         return "", None
 
+    interface_hull_residue_expanded = np.where(adjacency_matrix[1, interface_hull_nodes, :] == 1)
+    interface_hull_residue_expanded_nodes = np.unique(interface_hull_residue_expanded[1])
+
     selection_string = f"select i_hull_{pdb_id}, "
-    for atom in interface_hull_nodes:
+    for atom in interface_hull_residue_expanded_nodes:
         res_index = int(atom_encoding[atom][-1])
         atom_name = atom_names[atom]
         node_info = node_infos[res_index]
@@ -131,7 +128,7 @@ def write_selection_for_interface(pdb_id: str, cutoff: int = None, n_closest: in
     if use_dataloader:
         config = read_config("../../config.yaml")
         dataset = BoundComplexGraphs(config, "Dataset_v1", pdb_ids=[pdb_id], max_nodes=n_closest,
-                                     interface_distance_cutoff=cutoff, node_type="atom", load_from_disc=False,
+                                     interface_distance_cutoff=cutoff, node_type="atom",
                                      save_graphs=False, force_recomputation=True)
 
         graph_dict = dataset.get_graph_dict(pdb_id)
@@ -151,7 +148,7 @@ def write_selection_for_interface(pdb_id: str, cutoff: int = None, n_closest: in
 
     elif pdb_path is not None and chain_id2protein is not None:
         structure, header = read_file(pdb_id, pdb_path)
-        structure_info, node_infos, residue_atom_coordinates = get_residue_infos(structure, header, chain_id2protein)
+        structure_info, node_infos, residue_atom_coordinates = get_residue_infos(structure, chain_id2protein)
         distances, closest_atoms = get_distances(node_infos, residue_distance=False, ca_distance=False)
         atom_encoding, atom_names = get_atom_encodings(node_infos, structure_info, chain_id2protein)
         A = get_atom_edge_encodings(distances, atom_encoding, cutoff)
@@ -189,7 +186,7 @@ def write_connection_lines(pdb_id: str, cutoff: int = 5, graph_dict = None, use_
     if graph_dict is None and use_dataloader:
         config = read_config("../../config.yaml")
         dataset = BoundComplexGraphs(config, "Dataset_v1",  pdb_ids=[pdb_id],
-                                     interface_distance_cutoff=cutoff, node_type="residue", load_from_disc=False,
+                                     interface_distance_cutoff=cutoff, node_type="residue",
                                      save_graphs=False, force_recomputation=True)
 
         graph_dict = dataset.get_graph_dict(pdb_id)
@@ -197,7 +194,7 @@ def write_connection_lines(pdb_id: str, cutoff: int = 5, graph_dict = None, use_
 
     elif graph_dict is None and pdb_path is not None and chain_id2protein is not None:
         structure, header = read_file(pdb_id, pdb_path)
-        structure_info, residue_infos, residue_atom_coordinates = get_residue_infos(structure, header, chain_id2protein)
+        structure_info, residue_infos, residue_atom_coordinates = get_residue_infos(structure, chain_id2protein)
         distances, closest_residues = get_distances(residue_infos, residue_distance=False)
         A = get_residue_edge_encodings(distances, residue_infos, chain_id2protein, cutoff)
         interface = A[0, :, :]  - A[2, :, :]
@@ -231,7 +228,7 @@ def write_atom_connection_lines(pdb_id: str, cutoff: int = 5, use_dataloader: bo
     if use_dataloader:
         config = read_config("../../config.yaml")
         dataset = BoundComplexGraphs(config, "Dataset_v1",  pdb_ids=[pdb_id],
-                                     interface_distance_cutoff=cutoff, node_type="atom", load_from_disc=False,
+                                     interface_distance_cutoff=cutoff, node_type="atom",
                                      save_graphs=False, force_recomputation=True)
 
         graph_dict = dataset.get_graph_dict(pdb_id)
@@ -242,7 +239,7 @@ def write_atom_connection_lines(pdb_id: str, cutoff: int = 5, use_dataloader: bo
 
     elif pdb_path is not None and chain_id2protein is not None:
         structure, header = read_file(pdb_id, pdb_path)
-        structure_info, node_infos, residue_atom_coordinates = get_residue_infos(structure, header, chain_id2protein)
+        structure_info, node_infos, residue_atom_coordinates = get_residue_infos(structure, chain_id2protein)
         distances, closest_atoms = get_distances(node_infos, residue_distance=False, ca_distance=False)
         node_features, atom_names = get_atom_encodings(node_infos, structure_info, chain_id2protein)
         A = get_atom_edge_encodings(distances, node_features, cutoff)
@@ -280,7 +277,7 @@ def write_atom_connection_lines(pdb_id: str, cutoff: int = 5, use_dataloader: bo
 def load_pdb(pdb_id: str, file_path: str = None):
     if file_path is None:
         config = read_config("../../config.yaml")
-        dataset = BoundComplexGraphs(config, "Dataset_v1", pdb_ids=[pdb_id], load_from_disc=False, save_graphs=False)
+        dataset = BoundComplexGraphs(config, "Dataset_v1", pdb_ids=[pdb_id], save_graphs=False)
         _, pdb_path = get_resources_paths(config, "Dataset_v1")
         file_name = dataset.data_df[dataset.data_df["pdb"] == pdb_id]["abdb_file"].values[0]
         file_path = os.path.join(pdb_path, file_name)
@@ -295,9 +292,45 @@ def load_pdb(pdb_id: str, file_path: str = None):
 
 def start_pymol(pymol_path: str):
     from utils import PyMol
-    pm = PyMol(pymol_path)
+    pm = PyMol(pymol_path, mode=1)
 
     return pm
+
+
+def get_deeprefine_edges(pdb_id: str):
+    config = read_config("../../config.yaml")
+    dataset = DeepRefineBackboneInputs(config, "Dataset_v1", pdb_ids=[pdb_id], interface_hull_size= 7,
+                                 save_graphs=False, force_recomputation=True )
+
+    datapoint = dataset.load_data_point(pdb_id)
+
+    graph_dict = dataset.get_graph_dict(pdb_id)
+    A = graph_dict["adjacency_tensor"]
+    node_infos = graph_dict["residue_infos"]
+    atom_names = graph_dict["atom_names"]
+    node_features = graph_dict["node_features"]
+
+    edges = datapoint["graph"].edges()
+    selection_string = ""
+    for edge in zip(edges[0].tolist(), edges[1].tolist()):
+        atom1, atom2 = edge
+
+        #atom1_type = ID2ATOM[int(np.where(graph_dict["node_features"][atom1][23:-1] == 1)[0])]
+        atom1_type = atom_names[atom1]
+        atom1_residue = int(node_features[atom1][-1])
+        chain_id_1 = node_infos[atom1_residue]["chain_id"]
+        residue_id_1 = node_infos[atom1_residue]["residue_id"]
+
+        atom2_type = atom_names[atom2]
+        atom2_residue = int(node_features[atom2][-1])
+
+        chain_id_2 = node_infos[atom2_residue]["chain_id"]
+        residue_id_2 = node_infos[atom2_residue]["residue_id"]
+
+        selection_string += f"distance {pdb_id}_deeprefine_edges, ({pdb_id} and chain {chain_id_1} and resi {residue_id_1} " \
+                            f"and name {atom1_type}), ({pdb_id} and chain {chain_id_2} and resi {residue_id_2} and name {atom2_type}),100, 4 \n "
+
+    return selection_string
 
 
 def load_pdb_into_pymol(pm, pdb_id: str, cutoff: int = 5):
@@ -347,31 +380,41 @@ def main():
 
 
 def pdb_analysis():
-    pdb_id = "cr9114_newcal1999" # "3sdy"
-    file_path = "/home/fabian/Desktop/Uni/Masterthesis/ag_binding_affinity/resources/dms_phillips21/pdbs/cr9114_newcal1999.pdb"
+    pdb_id = "5dd0" # "3sdy"
+    file_path = '/home/fabian/Desktop/Uni/Masterthesis/ag_binding_affinity/results/cleaned_pdb/Dataset_v1/deeprefine/5DD0_1.pdb'
     load_command = load_pdb(pdb_id, file_path)
-    chain2protein = {"b": 1, "c": 1, "d": 0}
+    chain2protein = {"p": 1, "h": 0, "l": 0}
     selection, graph_dict = write_selection_for_interface(pdb_id, cutoff=5, pdb_path=file_path, chain_id2protein=chain2protein)
 
     interface_graph_connections = write_atom_connection_lines(pdb_id, 5, pdb_path=file_path, chain_id2protein=chain2protein)
-    #atom_connection = write_atom_connection_lines(pdb_id)
-    #residue_connection = write_connection_lines(pdb_id)
-    mutations = [('D', '209'), ('D', '210'), ('D', '165'), ('D', '187'), ('D', '241'), ('D', '194'), ('D', '212'),
-                 ('D', '193'), ('D', '164'), ('D', '211'), ('D', '222'), ('D', '166'), ('D', '230'), ('D', '219'),
-                 ('D', '192'), ('D', '206')]
 
-    #mutations = [('D', '230'), ('D', '219'), ('D', '222')]
-    mutations = [('D', '187'), ('D', '241'), ('D', '166'), ('D', '230')]
+    #mutations = [('D', '187'), ('D', '241'), ('D', '166'), ('D', '230')]
+    #mutation_select = select_residues(pdb_id, mutations, "Mutations")
 
-    mutation_select = select_residues(pdb_id, mutations, "Mutations")
+    deep_refine_edges = get_deeprefine_edges(pdb_id)
 
+    res_hull,_ = write_selection_for_atom_interface_hull(pdb_id, interface_cutoff = 5, hull_size = 7, pdb_path=file_path, chain_id2protein=chain2protein)
     expanded_interface_5 = "select expanded_5"  + selection[selection.find("_interface"):] + " expand 5"
-    expaned_interface_10 = "select expanded_10"  + selection[selection.find("_interface"):] + " expand 10"
+    expaned_interface_7 = "select expanded_10"  + selection[selection.find("_interface"):] + " expand 7"
+
+
+    pm = start_pymol("/home/fabian/Downloads/pymol/bin")
+    pm(load_command)
+    pm(res_hull)
+
+    for edge in interface_graph_connections.split("\n"):
+        pm(edge)
+    pm("hide label")
+
+    for edge in deep_refine_edges.split("\n"):
+        pm(edge)
+
     return
 
 
 if __name__ == "__main__":
-    #pdb_analysis()
+    pdb_analysis()
+    exit(0)
     #main()
 
     pdb_id = "1bj1"
