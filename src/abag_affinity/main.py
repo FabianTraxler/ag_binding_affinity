40ab4f51873f733da225a9f2d74920de0f25ac09
FabianTraxler <fabi.traxler@hotmail.com>
2022-11-14 14:59:01 +0100

    Clean-up main entry script

--- src/abag_affinity/main.py
+++ src/abag_affinity/main.py
@@ -1,12 +1,13 @@
 """This module provides all training utilities for the antibody-antigen binding affinity prediction"""
 import logging
+import random
 import sys
-from argparse import ArgumentParser, Namespace, Action
-from pathlib import Path
+from argparse import Namespace
 from typing import Dict
 import wandb
+import subprocess
 
-from abag_affinity.utils.config import read_config
+from abag_affinity.utils.argparse_utils import parse_args, enforced_node_type
 from abag_affinity.train import (bucket_train, cross_validation, model_train,
                                  pretrain_model)
 
@@ -16,20 +17,6 @@ training = {
     "pretrain_model": pretrain_model,
     "model_train": model_train,
     "cross_validation": cross_validation
-}
-
-model2data = {
-    "GraphConv": "BoundComplexGraphs",
-    "GraphAttention": "BoundComplexGraphs",
-    "FixedSizeGraphConv": "BoundComplexGraphs",
-    "DeepRefineBackbone": "DeepRefineInputs",
-    "DDGBackboneFC": "DDGBackboneInputs",
-    "KpGNN": "HeteroGraphs"
-}
-
-enforced_node_type = {
-    "DDGBackboneFC": "residue",
-    "DeepRefineBackbone": "atom"
 }
 
 
@@ -61,162 +48,65 @@ def logging_setup(args: Namespace):
     return main_logger
 
 
-class BooleanOptionalAction(Action):
-    """Taken from argparse Python >= 3.9 and added here to suppoert python < 3.9"""
-    def __init__(self,
-                 option_strings,
-                 dest,
-                 default=None,
-                 type=None,
-                 choices=None,
-                 required=False,
-                 help=None,
-                 metavar=None):
-
-        _option_strings = []
-        for option_string in option_strings:
-            _option_strings.append(option_string)
-
-            if option_string.startswith('--'):
-                option_string = '--no-' + option_string[2:]
-                _option_strings.append(option_string)
-
-        super().__init__(
-            option_strings=_option_strings,
-            dest=dest,
-            nargs=0,
-            default=default,
-            type=type,
-            choices=choices,
-            required=required,
-            help=help,
-            metavar=metavar)
-
-    def __call__(self, parser, namespace, values, option_string=None):
-        if option_string in self.option_strings:
-            setattr(namespace, self.dest, not option_string.startswith('--no-'))
-
-    def format_usage(self):
-        return ' | '.join(self.option_strings)
-
-
-def parse_args() -> Namespace:
-    """ CLI arguments parsing functionality
-
-    Parse all CLI arguments and set not available to default values
-
-    Returns:
-        Namespace: Class with all arguments
-    """
-
-    parser = ArgumentParser(description='CLI for using the abag_affinity module')
-
-    required = parser.add_argument_group('required arguments')
-    optional = parser.add_argument_group('optional arguments')
-
-    optional.add_argument("-t", "--train_strategy", type=str, help='The training strategy to use',
-                          choices=["bucket_train", "pretrain_model", "model_train", "cross_validation"],
-                          default="model_train")
-    optional.add_argument("-m", "--model_type", type=str, help='The type of model to train',
-                          choices=["GraphConv", "GraphAttention", "FixedSizeGraphConv", "DDGBackboneFC", "KpGNN",
-                                   "DeepRefineBackbone"], default="GraphConv")
-    optional.add_argument("-d", "--data_type", type=str, help='The type of dataset (graphs) to use for training',
-                          choices=["BoundComplexGraphs", "HeteroGraphs", "DeepRefineInputs",
-                                   "DDGBackboneInputs"], default="BoundComplexGraphs")
-    optional.add_argument("-b", "--batch_size", type=int, help="Batch size used for training", default=1)
-    optional.add_argument("-e", "--max_epochs", type=int, help="Max number of training epochs", default=100)
-    optional.add_argument("-lr", "--learning_rate", type=float, help="Initial learning rate", default=1e-4)
-    optional.add_argument("-p", "--patience", type=int,
-                          help="Number of epochs with no improvement until end of training",
-                          default=10)
-    optional.add_argument("-n", "--node_type", type=str, help="Type of nodes in the graphs", default="residue",
-                          choices=["residue", "atom"])
-    optional.add_argument("--max_num_nodes", type=int, help="Maximal number of nodes for fixed sized graphs",
-                          default=None)
-    optional.add_argument("--interface_hull_size", type=int, help="Size of the extension from interface to generate interface hull",
-                          default=None)
-    optional.add_argument("--scale_values", action=BooleanOptionalAction, help="Scale affinity values between 0 and 1",
-                          default=False)
-    optional.add_argument("-l", "--loss_function", type=str, help="Type of Loss Function", default="L1",
-                          choices=["L1", "L2"]
-                          )
-    optional.add_argument("-w", "--num_workers", type=int, help="Number of workers to use for data loading", default=0)
-    optional.add_argument("-wdb", "--use_wandb", action=BooleanOptionalAction, help="Use Weight&Bias to log training process",
-                          default=False)
-    optional.add_argument("--wandb_mode", type=str, help="Mode of Weights&Bias Process", choices=["online", "offline"],
-                          default="online")
-    optional.add_argument("--wandb_name", type=str, help="Name of the Weight&Bias logs", default="")
-    optional.add_argument("--init_sweep", action=BooleanOptionalAction, help="Use Weight&Bias sweep to search hyperparameter space",
-                          default=False)
-    optional.add_argument("--sweep_config", type=str, help="Path to the configuration file of the sweep",
-                          default=(Path(__file__).resolve().parents[1] / "config.yaml").resolve())
-    optional.add_argument("--sweep_id", type=str, help="The sweep ID to use for all runs")
-    optional.add_argument("--sweep_runs", type=int, help="Number of runs to perform in this sweep instance",
-                          default=30)
-    optional.add_argument("-v", "--validation_set", type=int, help="Which validation set to use", default=1,
-                          choices=[1, 2, 3])
-    optional.add_argument("-c", "--config_file", type=str,
-                          help="Path to config file for datasets and training strategies",
-                          default=(Path(__file__).resolve().parents[1] / "config.yaml").resolve())
-    optional.add_argument("--verbose", action=BooleanOptionalAction, help="Print verbose logging statements",
-                          default=False)
-    optional.add_argument("--preprocess_graph", action=BooleanOptionalAction,
-                          help="Compute graphs beforehand to speedup training (especially for DeepRefine",
-                          default=False)
-    optional.add_argument("--save_graphs", action=BooleanOptionalAction, help="Saves computed graphs to speed up training in later epochs",
-                          default=False)
-    optional.add_argument("--force_recomputation", action=BooleanOptionalAction,
-                          help="Force recomputation of graphs - deletes folder containing processed graphs",
-                          default=False)
-    optional.add_argument("--no_shuffle", action=BooleanOptionalAction,
-                          help="Shuffle train-dataloader",
-                          default=False)
-
-    args = parser.parse_args()
-    args.config = read_config(args.config_file)
-
-    if args.wandb_name == "":
-        args.wandb_name = f'{args.model_type}' \
-                          f' -d {args.data_type}' \
-                          f' -t {args.train_strategy}' \
-                          f' -n {args.node_type}'
-
-    # check arguments
-    if args.data_type != model2data[args.model_type]:
-        args.data_type = model2data[args.model_type]
-    if args.model_type in enforced_node_type and args.node_type != enforced_node_type[args.model_type]:
-        args.__dict__["node_type"] = enforced_node_type[args.model_type]
-
-    return args
-
-
 def run_sweep(args: Namespace, logger):
     import traceback
-    import shutil
-    import os
+
+    ATOM_NODES_MULTIPLIKATOR = 5
 
     def sweep_train():
-        wandb.init()
-        config = wandb.config
-        for param in config.keys():
-            args.__dict__[param] = config[param]
-            if param == "model_type":
-                args.__dict__["data_type"] = model2data[config[param]]
-                if config[param] in enforced_node_type:
-                    args.__dict__["node_type"] = enforced_node_type[config[param]]
-        logger.info(f"Performing {args.train_strategy} for {args.model_type} using {args.data_type}")
-        try:
-            training[args.train_strategy](args)
-            # remove all processed graphs to keep disk clean
-            if os.path.exists(args.config["cleaned_pdbs"]) and os.path.isdir(args.config["cleaned_pdbs"]):
-                shutil.rmtree(args.config["processed_graph_path"])
-            if os.path.exists(args.config["cleaned_pdbs"]) and os.path.isdir(args.config["cleaned_pdbs"]):
-                shutil.rmtree(args.config["cleaned_pdbs"])
+        run = wandb.init(mode=args.wandb_mode)
+        run_dir = run.dir[:-6]
 
+        try:
+            config = wandb.config
+            for param in config.keys():
+                if config[param] == "None":
+                    param_value = None
+                else:
+                    param_value = config[param]
+
+                if param == "max_num_nodes" and param_value is None and config["aggregation_method"] == "fixed_size":
+                    continue # ignore since it is manually overwritten below
+                if param == "node_type" and config["pretrained_model"] in enforced_node_type:
+                    continue # ignore since it is manually overwritten below
+
+                args.__dict__[param] = param_value
+                if param == "pretrained_model":
+                    if config[param] in enforced_node_type:
+                        args.__dict__["node_type"] = enforced_node_type[config[param]]
+                        config["node_type"] = enforced_node_type[config[param]]
+
+                if param == 'aggregation_method':
+                    if param_value == "fixed_size" and config["max_num_nodes"] == "None":
+                        max_num_nodes_values = [ int(value) for value in args.config["HYPERPARAMETER_SEARCH"]["parameters"]["max_num_nodes"]["values"] if value != "None"]
+                        args.max_num_nodes = random.choice(max_num_nodes_values)
+                        config["max_num_nodes"] = args.max_num_nodes
+
+            # adapt hyperparameter based on node type
+            if args.node_type == "atom" and args.max_num_nodes is not None:
+                args.max_num_nodes = int(args.max_num_nodes * ATOM_NODES_MULTIPLIKATOR)
+
+            # adapt batch size bases on node type
+            if args.node_type == "atom":
+                args.batch_size = int(args.batch_size / ATOM_NODES_MULTIPLIKATOR) + 1
+
+            # adapt learning rate bases on batch size
+            args.learning_rate = args.learning_rate * args.batch_size
+
+            args.tqdm_output = False # disable tqdm output to reduce log syncing
+
+            logger.info(f"Performing {args.train_strategy}")
+            training[args.train_strategy](args)
             wandb.finish(0)
-        except Exception:
+        except Exception as e:
+            # log errors before finishing job
+            logger.error(e)
             logger.error(traceback.print_exc())
             wandb.finish(-1)
+
+        if args.wandb_mode == "offline":
+            command = f'wandb sync --id {run.id} {run_dir}'
+            subprocess.run(command, shell=True)
 
     logger.info(f"Starting {args.sweep_runs} runs in this instance")
     wandb.agent(args.sweep_id, function=sweep_train, count=args.sweep_runs, project="abag_binding_affinity")
@@ -242,7 +132,7 @@ def main() -> Dict:
     if args.sweep_id is not None and args.sweep_runs > 0:
         run_sweep(args, logger)
     else:
-        logger.info(f"Performing {args.train_strategy} for {args.model_type} using {args.data_type}")
+        logger.info(f"Performing {args.train_strategy}")
         results = training[args.train_strategy](args)
 
         return results
