dad99388b8ccc10946f4bfa88cb359d43085b6d9
FabianTraxler <fabi.traxler@hotmail.com>
2022-12-30 11:33:07 +0100

    Add feature extraction methods

---
+++ src/abag_affinity/utils/feature_extraction.py
@@ -0,0 +1,107 @@
+"""Variables and functions are taken from https://github.com/595693085/DGraphDTA/blob/2b8a04d1e43d287c402d87bdeb5774ed9d6e98fe/data_process.py#L76 and slightly adapted
+
+Credit to:
+@Article{D0RA02297G,
+author ="Jiang, Mingjian and Li, Zhen and Zhang, Shugang and Wang, Shuang and Wang, Xiaofeng and Yuan, Qing and Wei, Zhiqiang",
+title  ="Drug–target affinity prediction using graph neural network and contact maps",
+journal  ="RSC Adv.",
+year  ="2020",
+volume  ="10",
+issue  ="35",
+pages  ="20701-20712",
+publisher  ="The Royal Society of Chemistry",
+doi  ="10.1039/D0RA02297G",
+url  ="http://dx.doi.org/10.1039/D0RA02297G",
+abstract  ="Computer-aided drug design uses high-performance computers to simulate the tasks in drug design{,} which is a promising research area. Drug–target affinity (DTA) prediction is the most important step of computer-aided drug design{,} which could speed up drug development and reduce resource consumption. With the development of deep learning{,} the introduction of deep learning to DTA prediction and improving the accuracy have become a focus of research. In this paper{,} utilizing the structural information of molecules and proteins{,} two graphs of drug molecules and proteins are built up respectively. Graph neural networks are introduced to obtain their representations{,} and a method called DGraphDTA is proposed for DTA prediction. Specifically{,} the protein graph is constructed based on the contact map output from the prediction method{,} which could predict the structural characteristics of the protein according to its sequence. It can be seen from the test of various metrics on benchmark datasets that the method proposed in this paper has strong robustness and generalizability."}
+
+"""
+
+import numpy as np
+
+
+def dic_normalize(dic):
+    max_value = dic[max(dic, key=dic.get)]
+    min_value = dic[min(dic, key=dic.get)]
+    interval = float(max_value) - float(min_value)
+    for key in dic.keys():
+        dic[key] = (dic[key] - min_value) / interval
+    dic['X'] = (max_value + min_value) / 2.0
+    return dic
+
+
+pro_res_table = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y',
+                 'X']
+
+pro_res_aliphatic_table = ['A', 'I', 'L', 'M', 'V']
+pro_res_aromatic_table = ['F', 'W', 'Y']
+pro_res_polar_neutral_table = ['C', 'N', 'Q', 'S', 'T']
+pro_res_acidic_charged_table = ['D', 'E']
+pro_res_basic_charged_table = ['H', 'K', 'R']
+
+res_weight_table = {'A': 71.08, 'C': 103.15, 'D': 115.09, 'E': 129.12, 'F': 147.18, 'G': 57.05, 'H': 137.14,
+                    'I': 113.16, 'K': 128.18, 'L': 113.16, 'M': 131.20, 'N': 114.11, 'P': 97.12, 'Q': 128.13,
+                    'R': 156.19, 'S': 87.08, 'T': 101.11, 'V': 99.13, 'W': 186.22, 'Y': 163.18}
+
+res_pka_table = {'A': 2.34, 'C': 1.96, 'D': 1.88, 'E': 2.19, 'F': 1.83, 'G': 2.34, 'H': 1.82, 'I': 2.36,
+                 'K': 2.18, 'L': 2.36, 'M': 2.28, 'N': 2.02, 'P': 1.99, 'Q': 2.17, 'R': 2.17, 'S': 2.21,
+                 'T': 2.09, 'V': 2.32, 'W': 2.83, 'Y': 2.32}
+
+res_pkb_table = {'A': 9.69, 'C': 10.28, 'D': 9.60, 'E': 9.67, 'F': 9.13, 'G': 9.60, 'H': 9.17,
+                 'I': 9.60, 'K': 8.95, 'L': 9.60, 'M': 9.21, 'N': 8.80, 'P': 10.60, 'Q': 9.13,
+                 'R': 9.04, 'S': 9.15, 'T': 9.10, 'V': 9.62, 'W': 9.39, 'Y': 9.62}
+
+res_pkx_table = {'A': 0.00, 'C': 8.18, 'D': 3.65, 'E': 4.25, 'F': 0.00, 'G': 0, 'H': 6.00,
+                 'I': 0.00, 'K': 10.53, 'L': 0.00, 'M': 0.00, 'N': 0.00, 'P': 0.00, 'Q': 0.00,
+                 'R': 12.48, 'S': 0.00, 'T': 0.00, 'V': 0.00, 'W': 0.00, 'Y': 0.00}
+
+res_pl_table = {'A': 6.00, 'C': 5.07, 'D': 2.77, 'E': 3.22, 'F': 5.48, 'G': 5.97, 'H': 7.59,
+                'I': 6.02, 'K': 9.74, 'L': 5.98, 'M': 5.74, 'N': 5.41, 'P': 6.30, 'Q': 5.65,
+                'R': 10.76, 'S': 5.68, 'T': 5.60, 'V': 5.96, 'W': 5.89, 'Y': 5.96}
+
+res_hydrophobic_ph2_table = {'A': 47, 'C': 52, 'D': -18, 'E': 8, 'F': 92, 'G': 0, 'H': -42, 'I': 100,
+                             'K': -37, 'L': 100, 'M': 74, 'N': -41, 'P': -46, 'Q': -18, 'R': -26, 'S': -7,
+                             'T': 13, 'V': 79, 'W': 84, 'Y': 49}
+
+res_hydrophobic_ph7_table = {'A': 41, 'C': 49, 'D': -55, 'E': -31, 'F': 100, 'G': 0, 'H': 8, 'I': 99,
+                             'K': -23, 'L': 97, 'M': 74, 'N': -28, 'P': -46, 'Q': -10, 'R': -14, 'S': -5,
+                             'T': 13, 'V': 76, 'W': 97, 'Y': 63}
+
+res_weight_table = dic_normalize(res_weight_table)
+res_pka_table = dic_normalize(res_pka_table)
+res_pkb_table = dic_normalize(res_pkb_table)
+res_pkx_table = dic_normalize(res_pkx_table)
+res_pl_table = dic_normalize(res_pl_table)
+res_hydrophobic_ph2_table = dic_normalize(res_hydrophobic_ph2_table)
+res_hydrophobic_ph7_table = dic_normalize(res_hydrophobic_ph7_table)
+
+
+def residue_features(residue):
+    res_property1 = [1 if residue in pro_res_aliphatic_table else 0, 1 if residue in pro_res_aromatic_table else 0,
+                     1 if residue in pro_res_polar_neutral_table else 0,
+                     1 if residue in pro_res_acidic_charged_table else 0,
+                     1 if residue in pro_res_basic_charged_table else 0]
+    res_property2 = [res_weight_table[residue], res_pka_table[residue], res_pkb_table[residue], res_pkx_table[residue],
+                     res_pl_table[residue], res_hydrophobic_ph2_table[residue], res_hydrophobic_ph7_table[residue]]
+    return np.array(res_property1 + res_property2)
+
+
+def atom_features(atom):
+    # 4 +4 +4 +1
+    return np.array(one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3]) +
+                    one_of_k_encoding_unk(atom.GetTotalNumHs(), [0, 1, 2, 3]) +
+                    one_of_k_encoding_unk(atom.GetImplicitValence(), [0, 1, 2, 3]) +
+                    [atom.GetIsAromatic()])
+
+
+# one ont encoding
+def one_of_k_encoding(x, allowable_set):
+    if x not in allowable_set:
+        raise Exception('input {0} not in allowable set{1}:'.format(x, allowable_set))
+    return list(map(lambda s: x == s, allowable_set))
+
+
+def one_of_k_encoding_unk(x, allowable_set):
+    '''Maps inputs not in the allowable set to the last element.'''
+    if x not in allowable_set:
+        x = allowable_set[-1]
+    return list(map(lambda s: x == s, allowable_set))
