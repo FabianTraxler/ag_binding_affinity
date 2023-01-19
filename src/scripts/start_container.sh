8d4b5abe270ab650323f0c58538de9f4951b9198
FabianTraxler <fabi.traxler@hotmail.com>
2023-01-19 15:46:49 +0100

    Adapt cluster parameters

--- src/scripts/start_container.sh
+++ src/scripts/start_container.sh
@@ -1,1 +1,1 @@
-srun --container-image=nvcr.io#nvidia/pytorch:22.02-py3 -N1 -q a100 -c 30 --gres=gpu:a100:1 --mem=60GB --pty bash
+srun --container-image=nvcr.io#nvidia/pytorch:22.02-py3 -N1 -q a100 -c 30 --gres=gpu:a100:1 --mem=100GB --pty bash
