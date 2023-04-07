1db8ca3017fc86ef424e552cf77d8cba3ae64d04
Moritz Schaefer <mail@moritzs.de>
2023-04-07 15:32:38 +0200

    Fix fine-tuning issue with WandB (needs the allow_val_change parameter...)

--- src/abag_affinity/train/wandb_config.py
+++ src/abag_affinity/train/wandb_config.py
@@ -25,9 +25,9 @@ def configure(args: Namespace) -> Tuple:
     else:
         run = wandb.init(project="abag_binding_affinity", mode="disabled")
 
+    wandb.config.update({
+        parameter: args.__dict__[parameter]
+        for parameter in ["batch_size", "max_epochs", "learning_rate", "patience", "max_num_nodes", "node_type", "num_workers", "validation_set"]
+    }, allow_val_change=True)
 
-    config = wandb.config
-    for parameter in ["batch_size", "max_epochs", "learning_rate", "patience", "max_num_nodes", "node_type", "num_workers", "validation_set"]:
-        config[parameter] = args.__dict__[parameter]
-
-    return wandb, config, use_wandb, run
+    return wandb, wandb.config, use_wandb, run
