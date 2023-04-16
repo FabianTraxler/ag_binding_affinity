b117880ad87eb21c79dd01d301480f3175c6b1e0
Moritz Schaefer <mail@moritzs.de>
2023-04-16 10:19:44 +0200

    Cross-editor compatible debug configuration (launch.json)

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,16 +1,4 @@
-((python-mode . ((eval . (blacken-mode -1))))
- (dap-register-debug-template
-  "Python :: Run ABAG affinity training"
-  (list :type "python"
-        :args " -t model_train -b 1 -e 2 -n residue --target_dataset abag_affinity_of_embeddings:absolute --validation_size 1 --gnn_type identity --pretrained_model IPA --aggregation_method interface_sum"
-        :cwd nil
-        :module nil
-        :program "/home/moritz/Projects/guided-protein-diffusion/modules/ag_binding_affinity/src/abag_affinity/main.py"
-        :request "launch"
-        :name "Python :: Run ABAG affinity training"))
-
- ;; TODO program is superseeded by target-module and we could enable '--relaxed_pdbs' at some point
- )
+((python-mode . ((eval . (blacken-mode -1)))))
 
 
 
