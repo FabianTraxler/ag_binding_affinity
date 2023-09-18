005fa5197412bf63550efa50f13d70a1c7d6feff
Moritz Schaefer <mail@moritzs.de>
2023-09-18 13:56:18 +0200

    Retry DMS single sweep (GNN)

--- src/experiments/DMS_single_prediciton_sweep/run.sh
+++ src/experiments/DMS_single_prediciton_sweep/run.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # SCRIPT="python ../../abag_affinity/main.py"
-SCRIPT="${DIR}/../../scripts/cluster_affinity"
+SCRIPT="$HOME/guided-protein-diffusion/scripts/cluster_affinity"
 
 $SCRIPT @${DIR}/base_args.txt
 
