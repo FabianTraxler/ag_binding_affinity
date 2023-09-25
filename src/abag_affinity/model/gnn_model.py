a38f72e3d477e93d3b92ad846779b9aa38b2193d
Marco <m.morik@tu-berlin.de>
2023-09-25 14:10:49 +0200

    only correct gradients of datalayer unfreezed from beginnin

--- src/abag_affinity/model/gnn_model.py
+++ src/abag_affinity/model/gnn_model.py
@@ -196,8 +196,11 @@ class AffinityGNN(pl.LightningModule):
         # Dataset-specific output layers
         self.dataset_names = dataset_names
         self.dataset_specific_layer = DatasetAdjustment(args.dms_output_layer_type, len(dataset_names), dataset_names)
-        if False:
-            self.dataset_specific_layer.requires_grad_(False)  # Freeze by default
+
+        self.dataset_specific_layer.requires_grad_(False)
+        # Making sure that only the correct gradients get updated (e.g. bias only)
+        self.dataset_specific_layer.unfreeze()
+
         self.scaled_output = scaled_output
 
         self.float()
