5d376e47355e84f0e054e57b43887259263a9104
Marco <m.morik@tu-berlin.de>
2023-10-05 17:06:41 +0200

    fix string bug

--- src/abag_affinity/train/utils.py
+++ src/abag_affinity/train/utils.py
@@ -688,7 +688,7 @@ def train_val_split(config: Dict, dataset_name: str, validation_set: Optional[in
         summary_path, _ = get_data_paths(config, dataset_name)
         summary_df = pd.read_csv(summary_path, index_col=0)
 
-        summary_df["validation"] = summary_df["validation"].fillna("").astype("str")
+        summary_df["validation"] = summary_df["validation"].fillna("")
 
         val_pdbs = summary_df.loc[summary_df["validation"] == validation_set, "pdb"]
         train_pdbs = summary_df.loc[(summary_df["validation"] != validation_set) & (~summary_df["test"]), "pdb"]
