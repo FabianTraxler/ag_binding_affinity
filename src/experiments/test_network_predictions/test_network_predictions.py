fb6f8913564841943d349a4f3acc5f610c2bc58b
Mihail Bogojeski <mihailbogojeski@gmail.com>
2024-01-31 15:01:26 +0100

    Reworking python script with new plots

--- src/experiments/test_network_predictions/test_network_predictions.py
+++ src/experiments/test_network_predictions/test_network_predictions.py
@@ -15,6 +15,9 @@ from tqdm import tqdm
 from sklearn.metrics import accuracy_score
 from functools import partial
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import colormaps
+from matplotlib.lines import Line2D
 # import pdb
 
 from abag_affinity.dataset import AffinityDataset
@@ -26,6 +29,7 @@ from abag_affinity.utils.visualize import plot_correlation
 from abag_affinity.utils.argparse_utils import read_args_from_file, parse_args
 from abag_affinity.train.utils import load_model
 from abag_affinity.model import regression_heads
+from abag_affinity.utils.pdb_processing import AMINO_ACIDS
 
 
 parser = argparse.ArgumentParser()
@@ -47,13 +51,6 @@ args.batch_size = 1
 args.cuda = False
 use_cuda = False
 device = 'cpu'
-
-train_data, val_datas = load_datasets(config, args.target_dataset, args.validation_set, args)
-
-model = AffinityGNN.load_from_checkpoint(meta_args.model_path, map_location='cpu')
-
-model.to('cpu')
-train_dataloader, val_dataloaders = get_bucket_dataloader(args, [train_data], val_datas)
 
 def get_test_dataloader(test_set: str, args: Namespace) -> DataLoader:
     if test_set == "abag_affinity":
@@ -93,8 +90,6 @@ def get_test_dataloader(test_set: str, args: Namespace) -> DataLoader:
 
     return dataloader
 
-
-test_dataloaders = {test_set: get_test_dataloader(test_set, args) for test_set in meta_args.test_sets}
 
 def get_dataset_predictions(model, dataloader, device):
     y_data = []
@@ -144,6 +139,17 @@ def get_dataset_predictions(model, dataloader, device):
     return preds_per_residue, y_data, y_pred
 
 
+test_dataloaders = {test_set: get_test_dataloader(test_set, args) for test_set in meta_args.test_sets}
+
+train_data, val_datas = load_datasets(config, args.target_dataset, args.validation_set, args)
+
+model = AffinityGNN.load_from_checkpoint(meta_args.model_path, map_location='cpu')
+
+model.to('cpu')
+train_dataloader, val_dataloaders = get_bucket_dataloader(args, [train_data], val_datas)
+
+
+
 dataloaders = {'Training': train_dataloader}
 dataloaders.update({'Validation' + str(i): val_dataloaders[i] for i in range(len(val_dataloaders))})
 dataloaders.update(test_dataloaders)
@@ -167,6 +173,58 @@ for set_name, dataloader in dataloaders.items():
     print('standard dev across residues', np.std(list(means_per_residue.values())), '\n')
     print('\n\n')
 
+# %%
+# Plot residue energy distributions
+
+
+fig = plt.figure(figsize=(20, 10))
+cmap_inds = np.linspace(0.0, 1.0, 3)
+colors = colormaps['viridis'](cmap_inds)
+
+
+offsets = np.linspace(-0.25, 0.25, len(results.keys()))
+for i, set_name in enumerate(results.keys()):
+    for res in results[set_name]['preds_per_residue'].keys():
+        res_preds = results[set_name]['preds_per_residue'][res]
+        plt.boxplot(res_preds, positions=[res + offsets[i]], boxprops=dict(color=colors[i], alpha=0.5), flierprops=dict(marker='.'))
+
+
+custom_lines = [Line2D([0], [0], color=colors[0], lw=4, alpha=0.5),
+                Line2D([0], [0], color=colors[1], lw=4, alpha=0.5),
+                Line2D([0], [0], color=colors[2], lw=4, alpha=0.5)]
+
+plt.legend(custom_lines, ['Training', 'Validation', 'Benchmark'], loc='lower right')
+plt.xticks(list(results['Training']['preds_per_residue'].keys()), [AMINO_ACIDS[i] for i in results['Training']['preds_per_residue'].keys()])
+plt.savefig('figures/aa_energy_predicitons.pdf', dpi=300)
+# plt.show()
+
+# %%
+# Plot residue energy distributions
+
+
+fig = plt.figure(figsize=(20, 10))
+cmap_inds = np.linspace(0.0, 1.0, 3)
+colors = colormaps['viridis'](cmap_inds)
+
+
+offsets = np.linspace(-0.25, 0.25, len(results.keys()))
+for i, set_name in enumerate(results.keys()):
+    for res in results[set_name]['preds_per_residue'].keys():
+        res_preds = results[set_name]['preds_per_residue'][res]
+        plt.boxplot(res_preds, positions=[res + offsets[i]], boxprops=dict(color=colors[i], alpha=0.5), flierprops=dict(marker='.'))
+
+
+custom_lines = [Line2D([0], [0], color=colors[0], lw=4, alpha=0.5),
+                Line2D([0], [0], color=colors[1], lw=4, alpha=0.5),
+                Line2D([0], [0], color=colors[2], lw=4, alpha=0.5)]
+
+plt.legend(custom_lines, ['Training', 'Validation', 'Benchmark'], loc='lower right')
+plt.xticks(list(results['Training']['preds_per_residue'].keys()), [AMINO_ACIDS[i] for i in results['Training']['preds_per_residue'].keys()])
+plt.ylim(0.011, 0.014)
+plt.savefig('figures/zoomed_in_aa_energy_predicitons.pdf', dpi=300)
+# plt.show()
+# %%
+
 
 train_mean = np.mean(results['Training']['y_data'])
 
@@ -183,3 +241,24 @@ mean_rmses = [results[set_name]['train_mean_rmse'] for set_name in dataloaders.k
 pred_rmses = [results[set_name]['pred_rmse'] for set_name in dataloaders.keys()]
 np.corrcoef(mean_rmses, pred_rmses)
 print('Correlation between train_mean_rmse and pred_rmse', np.corrcoef(mean_rmses, pred_rmses)[0, 1])
+
+fig = plt.figure()
+
+plt.axhline(y=train_mean, color='k', linestyle='--', label=f'Mean: {train_mean:.2f}')
+
+offsets = np.linspace(-0.15, 0.15, 2)
+cmap_inds = np.linspace(0.0, 1.0, 2)
+colors = colormaps['viridis'](cmap_inds)
+for i, set_name in enumerate(results.keys()):
+    plt.boxplot(results[set_name]['y_data'], positions=[i + offsets[0]], boxprops=dict(color=colors[0], alpha=0.5),
+                flierprops=dict(marker='.'), showmeans=True)
+    plt.boxplot(results[set_name]['y_pred'], positions=[i + offsets[1]], boxprops=dict(color=colors[1], alpha=0.5),
+                flierprops=dict(marker='.'), showmeans=True)
+
+custom_lines = [Line2D([0], [0], color=colors[0], lw=4, alpha=0.5),
+                Line2D([0], [0], color=colors[1], lw=4, alpha=0.5),
+                Line2D([0], [0], color='k', lw=1.5, linestyle='--')]
+
+plt.legend(custom_lines, ['data', 'pred', 'train mean'], loc='lower right')
+plt.xticks([0, 1, 2], ['Training', 'Validation', 'Benchmark'])
+plt.savefig('figures/dataset_distribution.pdf', dpi=300)
